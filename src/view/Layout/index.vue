cf88495382daa1f8749ebed1b58792e5e64c2c1d
‘98hb <w17339377595@163.com>
2022-02-16 19:48:52 +0800

    创建页面组件

---
+++ src/view/Layout/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>我是Layout</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+/* 中间内容区域 - 容器样式(留好上下导航所占位置) */
+.main {
+  padding-top: 46px;
+  padding-bottom: 50px;
+}
+</style>
